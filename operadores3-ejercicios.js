6b282ab2661b74d22368817e376a98aa2e9a2bcc
mariaabi82 <maria.abi1982@gmail.com>
2019-10-23 21:41:12 -0300

    agrego mes

--- operadores3-ejercicios.js
+++ operadores3-ejercicios.js
@@ -2,6 +2,7 @@
 const saludo = "Hola";
 const nombre = prompt("Por favor decime tu nombre");
 const año = prompt("Por favor decime tu año de nacimiento");
-let edad = (2019 - año)
+const mes = prompt("Por favor decime tu mes de nacimiento en número");
+let edad = mes > 11 && 2019 - año - 1 || mes < 11 && 2019 - año;
 console.log(nombre)
 alert(`Hola ${nombre}! Tu edad es ${edad}`)
