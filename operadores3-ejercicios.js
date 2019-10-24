34a6a2850ebe3eeac51c8eea836375fa139a97d7
mariaabi82 <maria.abi1982@gmail.com>
2019-10-23 21:56:25 -0300

    agrego solución de la GENIA Ivi

--- operadores3-ejercicios.js
+++ operadores3-ejercicios.js
@@ -6,3 +6,14 @@ const mes = prompt("Por favor decime tu mes de nacimiento en número");
 let edad = mes > 11 && 2019 - año - 1 || mes < 11 && 2019 - año;
 console.log(nombre)
 alert(`Hola ${nombre}! Tu edad es ${edad}`)
+
+
+// ** SOLUCIÓN IVI
+// const nombre = prompt("Escribí tu nombre");
+// const mes = prompt("Escribí el mes de tu cumpleaños");
+// const anio = prompt("Escribí tu año de nacimiento");
+// let mesActual = "octubre";
+// let anioActual = 2019;
+// let edad;
+// (mes === "noviembre" || mes === "diciembre") || alert("Tu edad es: " + (anioActual - anio));
+// (mes !=="noviembre" || mes !== "diciembre") && alert(`Tu edad es ${(anioActual-1) - anio}`);
