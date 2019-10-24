d2b6f7b1461cdfe20ce87cb9b6b14cf17ab88bf6
mariaabi82 <maria.abi1982@gmail.com>
2019-10-23 21:18:36 -0300

    subo ejercicio 1

---
+++ operadores3-ejercicios.js
@@ -0,0 +1,7 @@
+
+const saludo = "Hola";
+const nombre = prompt("Por favor decime tu nombre");
+const año = prompt("Por favor decime tu año de nacimiento");
+let edad = (2019 - año)
+console.log(nombre)
+alert(`Hola ${nombre}! Tu edad es ${edad}`)
